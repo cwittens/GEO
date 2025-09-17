319bee6cf671c6384e22825676ee3525efdaaeac
Collin Wittenstein <cwittens@students.uni-mainz.de>
2025-09-17 18:23:51 -0400

    added some temp information from kernel model

--- model-current/gpu_model/kernel_dbhe-coaxial.jl
+++ model-current/gpu_model/kernel_dbhe-coaxial.jl
@@ -150,9 +150,10 @@ xmin, xmax = 0.0, 1.0
 ymin, ymax = 0.0, 1.0
 zmin, zmax = 0.0, h2 + 1
 
-Nx = 101
-Ny = 101
-Nz = 81
+nn = 5
+Nx = nn * 100 + 1
+Ny = nn * 100 + 1
+Nz = nn * 80 + 1
 
 # Center of the pipe
 xc = (xmax - xmin) / 2
@@ -187,8 +188,8 @@ V[2, :, :, :] .= vy
 V[3, :, :, :] .= vz
 
 
-save(path, "velocity", V, gridx, gridy, gridz, 0)
-save(path, "diff_coeff", d, gridx, gridy, gridz, 0)
+# save(path, "velocity", V, gridx, gridy, gridz, 0)
+# save(path, "diff_coeff", d, gridx, gridy, gridz, 0)
 
 # change backend depending on hardware
 backend = CPU() # or ROCBackend() or CUDABackend() or CPU()
@@ -196,14 +197,14 @@ backend = CPU() # or ROCBackend() or CUDABackend() or CPU()
 cache = create_cache(backend=backend, d=d, vx=vx, vy=vy, vz=vz, gridx=gridx, gridy=gridy, gridz=gridz, r1=r1, t1=t1, r2=r2, ε=ε)
 ϕ_adapt = adapt(backend, ϕ)
 
-tspan = (0.0, 2400.0)
+tspan = (0.0, 30.0)
 prob = ODEProblem(rhs!, ϕ_adapt, tspan, cache)
 
 
 
 saveat = range(tspan..., 16)
 saveat = 0:2:30 # if it is not a integer, file names will be with decimal point
-callback, saved_values = save_and_print_callback(saveat, write_to_file=true)
+callback, saved_values = save_and_print_callback(saveat, write_to_file=false)
 @time sol = solve(prob, Tsit5(), save_everystep=false, abstol=1e-3, reltol=1e-3, callback=callback);
 
 for (i, t) in enumerate(saved_values.t)
@@ -224,3 +225,37 @@ for (i, t) in enumerate(saved_values_euler.t)
     println("using explicit euler: t = $(t)s, ϕ = $(round(temp, digits = 4))°C")
 end
 
+# using adaptive time integration: t = 0.0 s, ϕ = 20.0 °C
+# using adaptive time integration: t = 2.0 s, ϕ = 21.9061 °C
+# using adaptive time integration: t = 4.0 s, ϕ = 23.0856 °C
+# using adaptive time integration: t = 6.0 s, ϕ = 23.8155 °C
+# using adaptive time integration: t = 8.0 s, ϕ = 24.2669 °C
+# using adaptive time integration: t = 10.0 s, ϕ = 24.547 °C
+# using adaptive time integration: t = 12.0 s, ϕ = 24.7191 °C
+# using adaptive time integration: t = 14.0 s, ϕ = 24.8268 °C
+# using adaptive time integration: t = 16.0 s, ϕ = 24.8932 °C
+# using adaptive time integration: t = 18.0 s, ϕ = 24.9329 °C
+# using adaptive time integration: t = 20.0 s, ϕ = 24.9592 °C
+# using adaptive time integration: t = 22.0 s, ϕ = 24.9757 °C
+# using adaptive time integration: t = 24.0 s, ϕ = 24.9844 °C
+# using adaptive time integration: t = 26.0 s, ϕ = 24.9892 °C
+# using adaptive time integration: t = 28.0 s, ϕ = 24.9933 °C
+# using adaptive time integration: t = 30.0 s, ϕ = 24.9958 °C
+
+# nn = 4 with z = gridz[Nz-1] = 9.975
+# using adaptive time integration: t = 0.0 s, ϕ = 20.0 °C
+# using adaptive time integration: t = 2.0 s, ϕ = 22.3636 °C
+# using adaptive time integration: t = 4.0 s, ϕ = 23.6098 °C
+# using adaptive time integration: t = 6.0 s, ϕ = 24.2669 °C
+# using adaptive time integration: t = 8.0 s, ϕ = 24.6141 °C
+# using adaptive time integration: t = 10.0 s, ϕ = 24.796 °C
+# using adaptive time integration: t = 12.0 s, ϕ = 24.8935 °C
+# using adaptive time integration: t = 14.0 s, ϕ = 24.9426 °C
+# using adaptive time integration: t = 16.0 s, ϕ = 24.9709 °C
+# using adaptive time integration: t = 18.0 s, ϕ = 24.985 °C
+# using adaptive time integration: t = 20.0 s, ϕ = 24.9906 °C
+# using adaptive time integration: t = 22.0 s, ϕ = 24.9956 °C
+# using adaptive time integration: t = 24.0 s, ϕ = 24.9983 °C
+# using adaptive time integration: t = 26.0 s, ϕ = 24.9986 °C
+# using adaptive time integration: t = 28.0 s, ϕ = 24.9986 °C
+# using adaptive time integration: t = 30.0 s, ϕ = 24.9992 °C
